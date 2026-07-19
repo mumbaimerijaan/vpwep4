5842fea6be0d752d2884b775c1dde706132614ff
Sachin Mane <mumbaimerijaan@gmail.com>
2026-07-19 22:50:13 +0530

    fix: route stadium asset path to secure storage bucket URL

--- js/loaders.js
+++ js/loaders.js
@@ -3,10 +3,11 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 export function loadModel(scene) {
     return new Promise((resolve, reject) => {
         const loader = new GLTFLoader();
-        
+
         // Google Drive Usercontent direct CDN endpoint
-        const modelUrl = 'https://drive.usercontent.google.com/download?id=1QVgfP9BEXjRx7FbGbxSZfRIuCQs-WGGA&export=download';
-        
+        //const modelUrl = 'https://drive.usercontent.google.com/download?id=1QVgfP9BEXjRx7FbGbxSZfRIuCQs-WGGA&export=download';
+        const modelUrl = 'https://storage.googleapis.com/vpwc4bucket/assets/models/stadium.glb';
+
         console.log("Loading Al Janoub Stadium model from Google Drive Usercontent CDN...");
 
         loader.load(
@@ -34,11 +35,11 @@ function setupModel(model, scene) {
             node.receiveShadow = true;
         }
     });
-    
+
     // Position Al Janoub Stadium model
     model.position.set(49, 0, 19);
     model.rotation.set(0, 0, 0);
     model.scale.set(1.4, 1.4, 1.4);
-    
+
     scene.add(model);
 }
